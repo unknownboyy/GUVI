456a31fe89c5171e62ed0786f78a5f5de34e8ee2
Ankit Agrawal <ankitagrawal11b@gmail.com>
2018-10-14 14:31:59 +0530

    hello

--- guvi_2_1_2.py
+++ guvi_2_1_2.py
@@ -1,6 +1,6 @@
-def fact(n):
-    if n==0 or n==1:
+def my_function_to_find_factorial(number_to_find_factorial):
+    if number_to_find_factorial==0:
         return 1
-    return n*fact(n-1)
-n=int(input())
-print(fact(n))
+    return number_to_find_factorial*my_function_to_find_factorial(number_to_find_factorial-1)
+number_to_find_factorial_in_main_function=int(input())
+print(my_function_to_find_factorial(number_to_find_factorial_in_main_function))
